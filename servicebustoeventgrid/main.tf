492a81417c224be2467cedf53a3bc65eb0b0e55f
implodingduck <zsek320@gmail.com>
2022-12-19 09:58:33 -0600

    adding servicebustoeventgrid

---
+++ servicebustoeventgrid/main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.36.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "=3.1.0"
+    }
+    azapi = {
+      source = "azure/azapi"
+      version = "=1.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+locals {
+  func_name = "func${random_string.unique.result}"
+  location = "eastus"
+  gh_repo = "terraform-samples"
+  tags = {
+    "managed_by" = "terraform"
+    "repo"       = local.gh_repo
+  }
+}
+
+resource "random_string" "unique" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+
+data "azurerm_client_config" "current" {}
+
+data "azurerm_log_analytics_workspace" "default" {
+  name                = "DefaultWorkspace-${data.azurerm_client_config.current.subscription_id}-EUS"
+  resource_group_name = "DefaultResourceGroup-EUS"
+} 
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-${local.gh_repo}-${random_string.unique.result}-${local.location}"
+  location = local.location
+  tags = local.tags
+}

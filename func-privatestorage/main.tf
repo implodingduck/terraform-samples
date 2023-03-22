d353743c3769344d5a30930f593dad6206d26e46
implodingduck <zsek320@gmail.com>
2023-03-21 22:12:34 -0500

    setting pip_index_url

--- func-privatestorage/main.tf
+++ func-privatestorage/main.tf
@@ -254,7 +254,7 @@ resource "azurerm_linux_function_app" "func" {
     "BUILD_PROXY"  = "http://${azurerm_network_interface.example.private_ip_address}:8888/"
     "POST_BUILD_SCRIPT_PATH"         = "postbuild.sh"
     "PRE_BUILD_SCRIPT_PATH"         = "prebuild.sh"
-    "CUSTOM_REQUIREMENTSTXT_PATH"  = "emptyrequirements.txt"
+    "PIP_INDEX_URL" = "https://implodingduck.github.io"
   }
   identity {
     type         = "SystemAssigned"

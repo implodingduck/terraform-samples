d5f3d4b097dcb4ca126572577ad5b280d9543de8
implodingduck <zsek320@gmail.com>
2023-03-31 12:19:07 -0500

    adding a proxy server option

---
+++ proxy-server/main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.43.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "=3.1.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
+}
+
+locals {
+  func_name = "proxy${random_string.unique.result}"
+  loc_for_naming = "eastus"
+  tags = {
+    "managed_by" = "terraform"
+    "repo"       = "terraform-samples"
+  }
+}
+
+data "azurerm_resource_group" "rg" {
+  location = "eastus"
+  name = "rg-deleteme-srping"
+}
+
+data "azurerm_subnet" "vms" {
+  name                 = "proxy"
+  virtual_network_name = "vnet-spring"
+  resource_group_name  = data.azurerm_resource_group.rg.name
+
+}
+
+
+data "template_file" "vm-cloud-init" {
+  template = file("${path.module}/install-tinyproxy.sh")
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.vms.id
+    private_ip_address_allocation = "Dynamic"
+  }
+
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "vm-${local.func_name}-proxy"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = "azureuser"
+  custom_data         = base64encode(data.template_file.vm-cloud-init.rendered)
+
+  network_interface_ids = [
+    azurerm_network_interface.example.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts-gen2"
+    version   = "latest"
+  }
+  admin_ssh_key {
+    public_key = file("${path.module}/ssh_rsa.pub")
+    username   = "azureuser"
+  }
+}

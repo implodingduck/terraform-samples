bbd47e33b94bd5f3c07ef3b3e0b2ae905819c795
implodingduck <zsek320@gmail.com>
2023-04-18 15:02:41 -0500

    using unique pip for fw management ip

--- springapps-private/firewall.tf
+++ springapps-private/firewall.tf
@@ -6,6 +6,15 @@ resource "azurerm_public_ip" "fw" {
   sku                 = "Standard"
 }
 
+resource "azurerm_public_ip" "fwm" {
+  name                = "pip-fwm-${local.name}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+
 resource "azurerm_firewall" "this" {
   name                = "fw-${local.name}"
   location            = azurerm_resource_group.rg.location
@@ -22,6 +31,6 @@ resource "azurerm_firewall" "this" {
   management_ip_configuration {
     name = "managementconfiguration"
     subnet_id = azurerm_subnet.fwm.id
-    public_ip_address_id = azurerm_public_ip.fw.id
+    public_ip_address_id = azurerm_public_ip.fwm.id
   }
 }

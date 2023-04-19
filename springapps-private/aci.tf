864c46931e4cc89f45650953a20eda20ef84c3d9
implodingduck <zsek320@gmail.com>
2023-04-18 21:24:37 -0500

    ip type to private

--- springapps-private/aci.tf
+++ springapps-private/aci.tf
@@ -2,7 +2,7 @@ resource "azurerm_container_group" "aci" {
   name                = "aci-${local.name}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  ip_address_type     = "Public"
+  ip_address_type     = "Private"
   os_type             = "Linux"
   subnet_ids          = [azurerm_subnet.aci.id]
 

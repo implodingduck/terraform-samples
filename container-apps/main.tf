6a7217809036c4e6318225fde0768d43746d36a6
implodingduck <zsek320@gmail.com>
2023-05-16 14:04:07 -0500

    adding a container app environment

--- container-apps/main.tf
+++ container-apps/main.tf
@@ -33,6 +33,11 @@ locals {
 
 data "azurerm_client_config" "current" {}
 
+data "azurerm_log_analytics_workspace" "default" {
+  name                = "DefaultWorkspace-${data.azurerm_client_config.current.subscription_id}-EUS"
+  resource_group_name = "DefaultResourceGroup-EUS"
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "rg-container-apps-${random_string.unique.result}"
   location = "EastUS"
@@ -43,5 +48,15 @@ resource "random_string" "unique" {
   length  = 8
   special = false
   upper   = false
+}
+
+resource "azurerm_container_app_environment" "example" {
+  name                           = "my-environment"
+  location                       = azurerm_resource_group.rg.location
+  resource_group_name            = azurerm_resource_group.rg.name
+  log_analytics_workspace_id     = data.azurerm_log_analytics_workspace.default.id
+  infrastructure_subnet_id       = azurerm_subnet.apps.id
+  internal_load_balancer_enabled = true
+  tags                           = local.tags
 }
 
